c46f1ce9e345246c4cd12213201ae7f8f9392992
Hamad Safdar <hamadsafdar57@gmail.com>
2019-12-01 19:00:03 +0500

    hello world

--- models/user-model.js
+++ models/user-model.js
@@ -20,7 +20,8 @@ const userSchema = new Schema({
     city: { type: String, required: true },
     gender: { type: Boolean, required: true },
     dob: { type: Date, required: true },
-
+    //ref
+    posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}],
     // optional attributes
 
     profession: { type: String, default: null },
