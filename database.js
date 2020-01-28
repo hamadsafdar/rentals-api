0e2560f78a578341d7d8b8ed339195a19476ad87
Hamad Safdar <hamadsafdar57@gmail.com>
2020-01-28 21:03:59 +0500

    ok

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 
-const username = process.env.USERNAME || "maddi";
+const username =  "maddi";
 const password = process.env.PASWORD || "iiui";
 const dbname = process.env.DB_NAME || "rentalsdb";
 const port = process.env.PORT || 27017;
 const host = process.env.HOST_NAME || "localhost";
 
-const mongodb = 'mongodb+srv://maddi:iiui@cluster0-h8is1.mongodb.net/test?retryWrites=true&w=majority';
+//const mongodb = 'mongodb+srv://maddi:iiui@cluster0-h8is1.mongodb.net/test?retryWrites=true&w=majority';
 
 const url = `mongodb://${username}:${password}@${host}:${port}/${dbname}`;
 console.log(url);
@@ -24,5 +24,13 @@ module.exports.connect = async () => {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
+    })
+    .then(() => {
+        console.log(`Connected to DB!`);
+    })
+    .catch(err => {
+        console.log(`Connection Failed!`);
+        console.log(err);
     });
+    
 }
