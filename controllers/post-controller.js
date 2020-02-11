d6d321d6b4a80a7f5e9a6085c78e4d823f956f46
Hamad Safdar <hamadsafdar57@gmail.com>
2020-02-11 12:17:04 +0500

    Working on POST

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -46,14 +46,40 @@ module.exports.create_post = (req, res, next) => {
     newAddress.save().then().catch();
 
     //Post
+    newPost.house = newHouse._id;
+    newPost.address = newAddress._id;
+    newPost.createdBy = req.session.userId;
+    newPost.save().then().catch();
 
 };
 
 module.exports.acquire_post = (req, res, next) => {
+    Post.findById({ _id: req.body.id })
+        //orFail executes when required post is not found.
+        .orFail(() => {
+            res.status(404).message({
+                message: `Required post not found.`
+            });
+        })
+        .then(post => {
+            res.status(200).json({
+                message: `Heres the requested post`,
+                post: post
+            });
+        })
+        .catch(err => {
+            res.status(500).message({
+                message: `something went wrong while finfing the post!`
+            });
+            console.log(err.message + " 123");
+
+        });
 
 };
 
 module.exports.delete_post = (req, res, next) => {
+
+
 
 };
 
