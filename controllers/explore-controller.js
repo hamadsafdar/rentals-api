24ddc02292b24a46c3f9289d1d317c316f9764a3
Hamad Safdar <hamadsafdar57@gmail.com>
2020-02-13 13:53:02 +0500

    Initial Commit

---
+++ controllers/explore-controller.js
@@ -0,0 +1,6 @@
+//this controller finds nearby places for rent
+
+
+export function get_nearby_paces(req, res, next) {
+
+}
---
+++ routes/subroute-post.js
@@ -0,0 +1,8 @@
+// subroute to find posts of a specific user
+
+const express = require('express');
+const router = express.router();
+const postController = require ('../controllers/post-controller');
+
+
+router.get();
