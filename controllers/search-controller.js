4296d7c2bf5497d7990efd0474439fb666f0f7be
hamadsafdar <hamadsafdar57@gmail.com>
2020-03-03 20:54:30 +0500

    OKAY

---
+++ controllers/search-controller.js
@@ -0,0 +1,11 @@
+const User = require('../models/user-model');
+const Post = require('../models/post-model');
+
+module.exports.find_user = (req, res, next) => {
+
+};
+
+
+module.exports.find_post = (req, res, next) => {
+
+};
--- routes/discussion-routes.js
+++ routes/discussion-routes.js
@@ -2,20 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const forumController = require('../controllers/discussion-controller');
+const checkUser = require('../middleware/check-user');
 
 //TODO: Create a subroute
 
-router.get('/alltopics', forumController.get_all_topics);
+router.get('/topic/all', forumController.get_all_topics);
 
 router.get('/topic/:topicId', forumController.get_topic_comments);
 
-router.post('/topic', forumController.create_discussion_topic);
+router.post('/topic', checkUser.check_auth, forumController.create_discussion_topic);
 
-router.post('/comment', forumController.create_comment);
+router.post('/comment', checkUser.check_auth, forumController.create_comment);
 
-router.delete('/topic/:topicId', forumController.delete_topic);
+router.delete('/topic/:topicId', checkUser.check_auth, forumController.delete_topic);
 
-router.delete('/comment/:commentId', forumController.delete_comment);
+router.delete('/comment/:commentId', checkUser.check_auth, forumController.delete_comment);
 
 
 
